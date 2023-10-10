f09ffca4c091a7220caa8288abdfc294715cfaca
jeroperales <134175102+jeroperales@users.noreply.github.com>
2023-10-10 19:15:16 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
-//ESTRUCTURA
+// ESTRUCTURA
 typedef struct
 {
     int legajo;
@@ -16,17 +17,21 @@ typedef struct nodoArbol
     struct nodoArbol * der;
 } nodoArbol;
 
-
-//PROTOTIPADO
+// PROTOTIPADO
 nodoArbol* inicArbol();
 nodoArbol* crearNodoArbol(persona dato);
 nodoArbol* insertar(nodoArbol* arbol, persona dato);
-persona crearPersona ();
-void mostrarArbol (nodoArbol* arbol);
-
+persona crearPersona();
+int contarTerminal(nodoArbol* arbol);
+int buscar(nodoArbol* arbol, int edad);
+void mostrarArbol(nodoArbol* arbol);
 
 int main()
 {
+    int terminales = 0;
+    int grado1 = 0;
+    int flag = 0;
+    int edad = 0;
     char opc = 's';
     nodoArbol* arbol = inicArbol();
     nodoArbol* nodo;
@@ -42,7 +47,22 @@ int main()
         scanf("%c", &opc);
     }
 
-    mostrarArbol(arbol);
+    mostrarArbolInOrder(arbol);
+
+    terminales = contarTerminal(arbol);
+    printf("\n\nTERMINALES:");
+    printf("| %i |", terminales);
+
+    grado1 = contarGrado1(arbol);
+    printf("\n\nNODOS GRADO 1:");
+    printf("| %i |");
+
+    printf("\n\nINGRESE EDAD A BUSCAR\n");
+    scanf("%i", &edad);
+
+    flag = buscar(arbol, edad);
+    printf("\nFLAG = %d", flag);
+
     return 0;
 }
 
@@ -53,19 +73,19 @@ nodoArbol* inicArbol()
 
 nodoArbol* crearNodoArbol(persona dato)
 {
-    nodoArbol* aux = malloc(sizeof(nodoArbol));
-    aux->dato;
+    nodoArbol* aux = (nodoArbol*)malloc(sizeof(nodoArbol));
+    aux->dato = dato;
     aux->izq = NULL;
     aux->der = NULL;
 
     return aux;
 }
 
-nodoArbol* insertar(nodoArbol* arbol, persona dato) //ORDENADO POR EDAD
+nodoArbol* insertar(nodoArbol* arbol, persona dato) // ORDENADO POR EDAD
 {
     if (arbol != NULL)
     {
-        if(dato.edad < arbol->dato.edad)
+        if (dato.edad < arbol->dato.edad)
         {
             arbol->izq = insertar(arbol->izq, dato);
         }
@@ -81,30 +101,74 @@ nodoArbol* insertar(nodoArbol* arbol, persona dato) //ORDENADO POR EDAD
     return arbol;
 }
 
-persona crearPersona ()
+persona crearPersona()
 {
     persona aux;
-
-    printf("INGRESE NOMBRE: \n");
-    fflush(stdin);
-    gets(aux.nombre);
     printf("INGRESE EDAD: \n");
     fflush(stdin);
     scanf("%i", &aux.edad);
-    printf("INGRESE LEGAJO:\n");
-    scanf("%i", &aux.legajo);
 
     return aux;
 }
 
-void mostrarArbol (nodoArbol* arbol)
+void mostrarArbolInOrder(nodoArbol* arbol)
 {
     if (arbol != NULL)
     {
-
-        printf("%i", arbol->dato.edad);
-        mostrarArbol(arbol->izq);
-        printf("%i", arbol->dato.edad);
-
+        mostrarArbolInOrder(arbol->izq);
+        printf("| %i |", arbol->dato.edad);
+        mostrarArbolInOrder(arbol->der);
     }
 }
+
+int contarTerminal(nodoArbol* arbol)
+{
+    int i = 0;
+
+    if (arbol != NULL)
+    {
+        i = i + contarTerminal(arbol->izq);
+        i = i + contarTerminal(arbol->der);
+        if (arbol->der == NULL && arbol->izq == NULL)
+        {
+            i = i + 1;
+        }
+    }
+    return i;
+}
+
+int contarGrado1(nodoArbol* arbol)
+{
+    int i = 0;
+
+    if (arbol != NULL)
+    {
+        i = i + contarGrado1(arbol->izq);
+        i = i + contarGrado1(arbol->der);
+
+        if ((arbol->izq == NULL) ^ (arbol->der == NULL))
+        {
+            i++;
+        }
+    }
+    return i;
+}
+
+int buscar(nodoArbol* arbol, int edad)
+{
+    int i = 0;
+
+    if (arbol != NULL)
+    {
+        if (arbol->dato.edad == edad)
+        {
+            return 1;
+        }
+        else
+        {
+            i = buscar(arbol->der, edad) + buscar(arbol->izq, edad);
+        }
+    }
+    return i;
+}
+
